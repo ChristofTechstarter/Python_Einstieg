87d6a5cdd4afb183b0091876c117e5b48781d62e
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-11 13:42:00 +0100

    First Commit

---
+++ drive.py
@@ -0,0 +1,6 @@
+rot = False
+
+if not rot:
+    print("Du darfst Fahren!")
+else:
+    print("Du musst stehen bleiben")
---
+++ erstes_skript.py
@@ -0,0 +1,24 @@
+first_name = "Christof"
+last_name = "Gaßmann"
+
+print(first_name + " " + last_name)
+
+x = 5
+y = 10
+
+if x > y:
+    print("x ist größer als y")
+else:
+    print("x ist kleiner als y")
+
+Zahl1 = 5
+Zahl2 = 20
+
+print("Zahl 1 + Zahl2:", (Zahl1 + Zahl2))
+print("Zahl 2 - Zahl1:", (Zahl2 - Zahl1))
+print("Zahl 1 * Zahl2:", (Zahl1 * Zahl2))
+print("Zahl 2 / Zahl1:", (Zahl2 / Zahl1))
+
+print("Wie lautet dein Vorname?")
+vorname = input()
+print("Hallo, " + vorname)
