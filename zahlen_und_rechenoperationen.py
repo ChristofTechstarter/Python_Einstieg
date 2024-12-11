7f0f28ae796e4e36647c65afb39a290614da561f
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-11 14:31:14 +0100

    Create zahlen_und_rechenoperationen.py

---
+++ zahlen_und_rechenoperationen.py
@@ -0,0 +1,20 @@
+# Aufgabe 2.1
+
+a = 5
+b = 9
+
+print("Die Addition von a und b beträgt:", a + b)
+print("Die Subtraktion von a und b beträgt:", a - b)
+print("Die Multiplikation von a und b beträgt:", a * b)
+print("Die Division von a und b beträgt:", a / b)
+
+# Aufgabe 2.2
+
+note1 = 3
+note2 = 4
+note3 = 1
+note4 = 3
+
+noten = [note1, note2, note3, note4]
+
+print("Der Durchschnitt deiner Noten beträgt:", sum(noten) / len(noten))
