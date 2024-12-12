7be9fc2eb505b08cbd2819a250d65a4f21f203f8
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-12 13:46:47 +0100

    Create namensabfrage_mit_input.py

---
+++ namensabfrage_mit_input.py
@@ -0,0 +1,6 @@
+# 1.1 Fragt Den Vor und Nachnamen über input ab
+# 1.2 Nutzt print(f, um Euren Vornamen und Nachnamen auf der Konsole auszugeben
+
+vorname = input("Bitte gib deinen Vornamen ein:\n")
+nachname = input("Bitte gib deinen Nachnamen ein:\n")
+print(f"Hallo, {vorname} {nachname}")
