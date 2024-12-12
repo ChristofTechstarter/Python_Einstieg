ca2d56d1e162aa6d2b00c1b5ee2c87aa7517ef88
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-12 14:25:31 +0100

    Create einfache_if_bedingung.py

---
+++ einfache_if_bedingung.py
@@ -0,0 +1,12 @@
+# Schreibe ein Programm, das eine Zahl vom Benutzer einliest und überprüft, ob sie positiv,
+# negativ oder null ist.
+
+zahl = int(input("Bitte nenne mir eine Zahl: \n"))
+
+if zahl < 0:
+    print("Das ist eine negative Zahl!")
+else:
+    print("Das ist eine Positive Zahl!")
+
+if zahl == 0:
+    print("Deine Zahl ist 0!")
