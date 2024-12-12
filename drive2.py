f07a7f64f1c2321c06890139827bc33ef2ecd1bc
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-12 09:42:55 +0100

    Update drive2.py

--- drive2.py
+++ drive2.py
@@ -1,8 +1,8 @@
 def ampelsteuerung(farbe):
-    print(f"Die Ampel zeigt {farbe}")
+    print(f"Die Ampel zeigt {farbe}!")
     if farbe == "rot":
         print("Du musst stehen bleiben!")
     else:
-        print("Du darfst fahren")
+        print("Du darfst fahren!")
 
-ampelsteuerung("rot")
+ampelsteuerung("grün")
