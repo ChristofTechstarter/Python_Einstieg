5b7022ca73b3455bebc22fdf964051b5f7f180f1
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-12 08:46:47 +0100

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,7 +1,8 @@
 print("Welche Farbe zeigt die Ampel? (Rot, Gelb oder Grün)")
 farbe = input()
+farbe_klein = farbe.lower()
 
-if  farbe == "Rot":
+if  farbe_klein == "rot":
     print("Du musst stehen bleiben!")
 else:
     print("Du darfst fahren!")
