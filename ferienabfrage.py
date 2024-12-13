89e52a96fe73303549af7c81e0d4164618ce46d1
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-13 13:43:34 +0100

    Create ferienabfrage.py

---
+++ ferienabfrage.py
@@ -0,0 +1,60 @@
+# Ferien:
+# Winterferien 2024
+# 24.12.2024 - 01.01.2025
+# •4 Urlaubstage
+# • Letzter Unterrichtstag: 23.12.2024
+# • Erster Unterrichtstag nach den Ferien ist der 02.01.2025
+
+# Osterferien 2025
+# 18.04.2025 - 21.04.2025
+# • 0 Urlaubstage
+# • Letzter Unterrichtstag: 17.04.2025
+# • Erster Unterrichtstag nach den Ferien ist der 22.04.2025
+
+# Sommerferien 2025
+# 11.08.2025 - 19.08.2025
+# • 7 Urlaubstage
+# • Letzter Unterrichtstag: 08.08.2025
+# • Erster Unterrichtstag nach den Ferien ist der 20.08.2025
+
+# Winterferien 2025
+# 24.12.2025 - 01.01.2026
+# • 4 Urlaubstage
+# • Letzter Unterrichtstag: 23.12.2024
+# • Erster Unterrichtstag nach den Ferien ist der 02.01.2025
+
+# Feiertage:
+# Tag der deutschen Einheit
+# 03.10.2024
+# Reformationstag
+# 31.10.2024
+# 1. Weihnachtstag
+# 25.12.2024
+# 2. Weihnachtstag
+# 26.12.2024
+# Neujahr
+# 01.01.2025
+# Karfreitag
+# 18.04.2025
+# Ostermontag
+# 21.04.2025
+# Tag der Arbeit
+# 01.05.2025
+# Christi Himmelfahrt
+# 29.05.2025
+# Pfingstmontag
+# 09.06.2025
+# Tag der deutschen Einheit
+# 03.10.2025
+# Reformationstag
+# 31.10.2025
+# 1. Weihnachtstag
+# 25.12.2025
+# 2. Weihnachtstag
+# 26.12.2025
+# Neujahr
+# 01.01.2026
+
+feiertage = "03.10.2024" + "31.10.2024"
+
+print("feiertage")
