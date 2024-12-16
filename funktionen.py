6dabaa7539d0f2e9db484a9a67e997b201004f69
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-16 12:29:42 +0100

    Create funktionen.py

---
+++ funktionen.py
@@ -0,0 +1,10 @@
+# schreibe eine funktion, den den User nach dem Alter fragt
+# und auf der Konsole ausgibt
+
+
+def useralter():
+    alter = int(input("wie alt bist du? "))
+    print(f"Du bist {alter} Jahre alt")
+
+
+useralter()
